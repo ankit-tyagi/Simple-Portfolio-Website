c1f19b7485cb703a784b46f46a55721c5eb8b60a
Ankit Tyagi <ankittyaagi@gmail.com>
2019-05-15 12:20:22 +0530

    Added Typed Js and added type feature

--- js/script.js
+++ js/script.js
@@ -6,4 +6,12 @@ $(document).ready(function() {
 		pagination: false
 	});
 
+	var typed = new Typed(".typed", {
+		strings: ["Software Development Engineer.", "Machine Learning Engineer", "Web Developer"],
+		typeSpeed: 60,
+		loop: true,
+		startDelay: 500,
+		showCursor: false
+	});
+
 });
