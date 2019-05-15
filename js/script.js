17f5e2321d65d60e05f63fa971303aeb202df0b6
Ankit Tyagi <ankittyaagi@gmail.com>
2019-05-15 16:04:28 +0530

    Many Changes

--- js/script.js
+++ js/script.js
@@ -14,4 +14,38 @@ $(document).ready(function() {
 		showCursor: false
 	});
 
+	$('.owl-carousel').owlCarousel({
+	    loop:true,
+	    items: 5,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        480:{
+	            items:2
+	        },
+	        768:{
+	            items:3
+	        },
+	        938:{
+	            items:4
+	        },
+	        1240:{
+	            items:5
+	        }
+	    }
+	});
+
+	$('.chart').easyPieChart({
+        easing: 'easeInOut',
+        barColor: '#fff',
+        trackColor: false,
+        scaleColor: false,
+        lineWidth: 4,
+        size: 152,
+        onStep: function(from, to, percent) {
+        	$(this.el).find('.percent').text(Math.round(percent));
+        }
+    });
+
 });
